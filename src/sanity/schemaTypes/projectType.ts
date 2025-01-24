5eac7bd7ab8db10ac856542fe7fc3212ac5226f5
linzheng <alidlg9045@126.com>
2025-01-24 15:52:09 +0800

    feat: add sanity

---
+++ src/sanity/schemaTypes/projectType.ts
@@ -0,0 +1,49 @@
+import { defineType } from 'sanity'
+
+export const projectType = defineType({
+  name: 'project',
+  title: 'Project',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+      },
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: 'alt',
+          title: 'Alt',
+          type: 'string',
+        },
+      ],
+    },
+    {
+      name: 'url',
+      title: 'URL',
+      type: 'url',
+    },
+    {
+      name: 'content',
+      title: 'Content',
+      type: 'array',
+      of: [{ type: 'block' }],
+    },
+  ],
+})
+
