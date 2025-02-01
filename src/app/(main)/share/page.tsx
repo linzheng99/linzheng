2206b900da5ef89bfc10b5c4e733433f13d65e80
linzheng <alidlg9045@126.com>
2025-02-01 19:39:02 +0800

    feat: share sign

--- src/app/(main)/share/page.tsx
+++ src/app/(main)/share/page.tsx
@@ -1,4 +1,10 @@
+import ShareSign from "@/features/share/components/share-sign";
+
 export default function SharePage() {
-  return <div>Share</div>;
+  return (
+    <div className="mt-10 w-full min-h-screen">
+      <ShareSign />
+    </div>
+  )
 }
 
---
+++ src/features/share/components/share-sign.tsx
@@ -0,0 +1,13 @@
+export default function ShareSign() {
+  return (
+    <div className="w-full py-10 grid grid-cols-12">
+      <div className="col-span-4 flex items-center">
+        <span className="text-5xl font-black">SHARE</span>
+      </div>
+      <div className="col-span-8 flex justify-end items-end">
+        <p className="text-sm text-gray-300">
+        </p>
+      </div>
+    </div>
+  )
+}
